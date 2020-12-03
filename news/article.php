831775b4ffb757ad5c491fab97e23dd1d0a0ca54
tonyhappystyle <tonyhappystyle@163.com>
2020-12-03 23:19:37 +0800

    测试微信转发（第1次）

--- news/article.php
+++ news/article.php
@@ -5,7 +5,9 @@ include $_SERVER['DOCUMENT_ROOT'].'/footerWrapper.php';
 include $_SERVER['DOCUMENT_ROOT'].'/news.php';
 
 $id = isset($_GET['id'])?$_GET['id']:'0';
-
+require_once $_SERVER['DOCUMENT_ROOT']."/jssdk.php";
+$jssdk = new JSSDK("wx5c60b88691c0d193", "fa45f037203b0f67bdf7cacb92f10cc6");
+$signPackage = $jssdk->GetSignPackage();
 ?>
 <!doctype html>
 <html lang="zh-CN">
@@ -125,4 +127,38 @@ $id = isset($_GET['id'])?$_GET['id']:'0';
     <?=$footerWrapper?>
 </div>
 </body>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script>
+  /*
+   * 注意：
+   * 1. 所有的JS接口只能在公众号绑定的域名下调用，公众号开发者需要先登录微信公众平台进入“公众号设置”的“功能设置”里填写“JS接口安全域名”。
+   * 2. 如果发现在 Android 不能分享自定义内容，请到官网下载最新的包覆盖安装，Android 自定义分享接口需升级至 6.0.2.58 版本及以上。
+   * 3. 常见问题及完整 JS-SDK 文档地址：http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html
+   *
+   * 开发中遇到问题详见文档“附录5-常见错误及解决办法”解决，如仍未能解决可通过以下渠道反馈：
+   * 邮箱地址：weixin-open@qq.com
+   * 邮件主题：【微信JS-SDK反馈】具体问题
+   * 邮件内容说明：用简明的语言描述问题所在，并交代清楚遇到该问题的场景，可附上截屏图片，微信团队会尽快处理你的反馈。
+   */
+  wx.config({
+    debug: true,
+    appId: '<?php echo $signPackage["appId"];?>',
+    timestamp: <?php echo $signPackage["timestamp"];?>,
+    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+    signature: '<?php echo $signPackage["signature"];?>',
+    jsApiList: [
+      // 所有要调用的 API 都要加到这个列表中
+    ]
+  });
+  wx.ready(function () {
+var shareData = {
+title: '中国船舶工业集团公司',
+desc: '中国船舶工业集团公司',
+link: 'http://www.flashtek.com.cn/cssc/index.php',
+imgUrl: 'http://www.flashtek.com.cn/cssc/img/logo.png'
+};
+wx.onMenuShareAppMessage(shareData);
+wx.onMenuShareTimeline(shareData);
+  });
+</script>
 </html>
